8f4f9eb1e227c8a32bff57fbc82a97b187487f58
Haseeb zahid <haseebzahid6@gmail.com>
2022-05-16 15:07:16 +0500

    git ignore

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,14 @@ const url = process.env.MONGO_URL;
 const port = process.env.PORT;
 
 
+
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
     console.log("Connected to the database");
+}).catch((err) => {
+    console.log("Connection failed", err);
 })
 
 
