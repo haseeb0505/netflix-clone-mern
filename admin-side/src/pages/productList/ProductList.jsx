ff4605bc9ba9ff0dbd2714988f71266fac6cbe0b
Haseeb zahid <haseebzahid6@gmail.com>
2022-05-24 22:47:07 +0500

    Movie context

--- admin-side/src/pages/productList/ProductList.jsx
+++ admin-side/src/pages/productList/ProductList.jsx
@@ -1,14 +1,14 @@
 import "./productList.css";
 import { DataGrid } from "@material-ui/data-grid";
 import { DeleteOutline } from "@material-ui/icons";
-import { productRows } from "../../dummyData";
+// import { productRows } from "../../dummyData";
 import { Link } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { MovieContext } from "../../context/movieContext/MovieContext";
 import { getMovies, deleteMovies } from "../../context/movieContext/apiCalls";
 
 export default function ProductList() {
-  const [data, setData] = useState(productRows);
+  // const [data, setData] = React.useState(productRows);
   const { movies, dispatch } = useContext(MovieContext);
 
   useEffect(() => {
